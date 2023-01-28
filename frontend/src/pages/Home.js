401fb4a8cb13938daacd76b4854dcbe8cfa34380
Thomas Harlow <tharl001@odu.edu>
2023-01-28 17:32:55 -0500

    Added the Navbar with pages but nothing on the pages

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,9 @@
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+      <div>
+        <h1>Home</h1>
+        <h2>This is the home page</h2>
+        <img src={require('./logo.png')} height={300} width={300} />
+      </div>
+    )
   }
