dc6898003dc5018bcaa529f14dfff49995e61bd6
Yao Wang <ywang011@odu.edu>
2023-02-23 22:35:49 -0500

    map the photos

--- frontend/src/components/common/TeamPhoto.js
+++ frontend/src/components/common/TeamPhoto.js
@@ -1,14 +1,57 @@
 import React from "react"
-import { Image } from "react-bootstrap"
+import Image from "react-bootstrap/Image"
 
 const images = [
-  { name: "image1", path: "../../assets/images/e.jpg" },
-  { name: "image2", path: "../../assets/images/jo.jpg" },
-  { name: "image3", path: "../../assets/images/e.jpg" },
+  {
+    name: "Estefania Barron",
+    role: "role",
+    description: "This is the first image",
+    path: require("../../assets/images/e.jpg"),
+  },
+  {
+    name: "Joshua Wang",
+    role: "role",
+    description: "This is the first image",
+    path: require("../../assets/images/jo.jpg"),
+  },
+  {
+    name: "Natalie Mohun",
+    role: "role",
+    description: "This is the first image",
+    path: require("../../assets/images/n.jpg"),
+  },
+  {
+    name: "Thomas Harlow",
+    role: "role",
+    description: "This is the first image",
+    path: require("../../assets/images/t.png"),
+  },
+  {
+    name: "Vaelyn Dulce",
+    role: "role",
+    description: "This is the first image",
+    path: require("../../assets/images/v.jpg"),
+  },
+  {
+    name: "Yao Wang",
+    role: "role",
+    description: "This is the first image",
+    path: require("../../assets/images/y.png"),
+  },
 ]
-
 function TeamPhoto() {
-  return <div>1</div>
+  //   const imagePaths = Object.values(images)
+  return (
+    <div className="d-flex flex-wrap text-center">
+      {images.map((image, index) => (
+        <div key={index} className="col-md-4 mb-3">
+          <Image src={image.path} width={200} className="rounded m-2" />
+          <h5 className="my-2">{image.name}</h5>
+          <p>{image.role}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default TeamPhoto
