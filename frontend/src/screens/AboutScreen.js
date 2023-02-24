938c938fcfa86875f198d3d9fa996de2ae1e56c7
Vaelyn <vdulc001@odu.edu>
2023-02-24 17:55:23 -0500

    Grammar and spacing changed on About Page

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -1,31 +1,32 @@
 import React from "react"
 import { Container } from "react-bootstrap"
 import { MDBTypography, MDBIcon } from "mdb-react-ui-kit"
-import { Link } from "react-router-dom"
+//import { Link } from "react-router-dom"
 import TeamPhoto from "../components/common/TeamPhoto"
 function AboutScreen() {
   return (
     <Container className="py-3">
       <MDBTypography listUnStyled className="mb-0">
-        <h1>Our mission is to provide an online platform that</h1>
+        <h1>About Us</h1>
+        <h4 className="mt-5">Our mission is to provide an online platform that...</h4>
         <li className="mb-1">
           <MDBIcon icon="check-circle" className="me-2 text-success" />
-          Generate more traffic for Independent bookstores
+          Generates more traffic for independent bookstores
         </li>
         <li className="mb-1">
           <MDBIcon icon="check-circle" className="me-2 text-success" />
-          Provide best possible search results for book-lovers
+          Provides the best possible search results for book-lovers
         </li>
         <li className="mb-1">
           <MDBIcon icon="check-circle" className="me-2 text-success" />
-          Book recommendations
+          Provides personalized book recommendations
         </li>
       </MDBTypography>
-      <Link to="/" style={{ textDecoration: "none" }}>
+      {/* <Link to="/" style={{ textDecoration: "none" }}> */}
         <h4 className="mt-5">
-          <span>Meet the team</span> <i className="fa-solid fa-greater-than fa-xs"></i>
+          Meet the Team
         </h4>
-      </Link>
+      {/* </Link> */}
       <TeamPhoto />
     </Container>
   )
