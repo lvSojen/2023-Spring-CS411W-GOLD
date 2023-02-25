fad831ae4dab1f690a68c282d35f105071b45b1e
Yao Wang <ywang011@odu.edu>
2023-02-24 22:23:06 -0500

    ♻️ (PR): shorten the magrin top for the titile

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -8,7 +8,9 @@ function AboutScreen() {
     <Container className="py-3">
       <MDBTypography listUnStyled className="mb-0">
         <h1>About Us</h1>
-        <h4 className="mt-5">Our mission is to provide an online platform that...</h4>
+        <h4 className="mt-3">
+          Our mission is to provide an online platform that...
+        </h4>
         <li className="mb-1">
           <MDBIcon icon="check-circle" className="me-2 text-success" />
           Generates more traffic for independent bookstores
@@ -23,9 +25,7 @@ function AboutScreen() {
         </li>
       </MDBTypography>
       {/* <Link to="/" style={{ textDecoration: "none" }}> */}
-        <h4 className="mt-5">
-          Meet the Team
-        </h4>
+      <h4 className="mt-5">Meet the Team</h4>
       {/* </Link> */}
       <TeamPhoto />
     </Container>
