cd0ec572cb51de89b03102b8e5f53c2582d48f5b
Vaelyn <vdulc001@odu.edu>
2023-03-18 00:45:44 -0400

    Created recommendation page

---
+++ frontend/src/screens/RecommendationsScreen.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Container } from "react-bootstrap"
+import './Screens.css';
+
+function RecommendationsScreen() {
+  return (
+    <Container className="py-3">
+      <h1>Recommendations</h1>
+    </Container>
+  )
+}
+
+export default RecommendationsScreen
