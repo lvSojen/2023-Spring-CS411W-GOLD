d9f0401e0247c9af99eba3ec2480892d43e0c2ad
Vaelyn <vdulc001@odu.edu>
2023-03-20 12:21:40 -0400

    Add recommendation image and component

---
+++ frontend/src/components/common/RecData.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Image from "react-bootstrap/Image"
+import { Card, Container } from "react-bootstrap"
+
+function RecData()
+{
+  return (
+    <Container className="text-center">
+       <Card>
+       </Card>
+    </Container>
+  )
+}
+export default RecData
