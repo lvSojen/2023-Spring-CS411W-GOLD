105a6449ff6efdf8e38415ae97c4f1942af89042
jwangCSProject <jwang010@odu.edu>
2023-04-16 13:15:27 -0400

    Added FAQ section to BookedSearch website

---
+++ frontend/src/screens/FAQScreen.js
@@ -0,0 +1,13 @@
+import React from "react"
+import FAQ from "../components/common/FAQ"
+import { Container } from "react-bootstrap"
+
+function FAQScreen() {
+  return (
+    <Container className="py-3">
+      <FAQ/>
+    </Container>
+  )
+}
+
+export default FAQScreen
